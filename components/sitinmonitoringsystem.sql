71b2c6e9fdce25e398b47e849c4185c7f57e5bd0
BrylDG <brylgorgonio@gmail.com>
2025-03-24 23:40:02 +0800

    updated database

--- components/sitinmonitoringsystem.sql
+++ components/sitinmonitoringsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2025 at 04:20 PM
+-- Generation Time: Mar 24, 2025 at 04:38 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
